5cce51b880ce28f2012c90e52282c5cce8e93b86
quick-progress <georgiy.kuznetsov.19@mail.ru>
2020-06-25 00:48:48 +0600

    Добавлен интервал закрытия модального окна  5 сек

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,9 @@ var modal = document.querySelector('#modal');
 var close = document.querySelector('#close');
 button.addEventListener('click', function () {
     modal.classList.add('modal_active');
+    setTimeout(closeModal, 5000);
 });
-close.addEventListener('click', function () {
+function closeModal() {
     modal.classList.remove('modal_active');
-});
+};
+close.addEventListener('click', closeModal());
