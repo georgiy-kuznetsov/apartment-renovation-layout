cd93d53169477c6546b8cc992d15a1caecef7e55
quick-progress <georgiy.kuznetsov.19@mail.ru>
2020-06-25 23:33:05 +0600

    Добавил кнопку прокрутки наверх. Убрал закрытие модального окначерез 5 сек после открытия

---
+++ src/js/modal.js
@@ -0,0 +1,22 @@
+$(function() {
+    var button = $('#button');
+    var modal = $('#modal');
+    var close = $('#close');
+    button.on('click', function() {
+        modal.addClass('modal_active');
+    });
+    close.on('click', function() {
+        modal.removeClass('modal_active');
+    });
+
+    //Кнопка прокрутки наверх
+    $(window).on("scroll", function() {
+        if ($(window).scrollTop() > 50) $('#scroll-button')
+        .addClass('scroll-button_visible');
+            else $('#scroll-button').removeClass('scroll-button_visible');
+        });
+    var scrollButton = $('#scroll-button');
+    scrollButton.on('click', function() {
+        $('html, body').animate({scrollTop:0}, 'slow');
+    });
+});
