c4b21bf54776ac24514927323a76727ff4e3bbb7
quick-progress <georgiy.kuznetsov.19@mail.ru>
2020-07-12 20:03:37 +0600

    Добавлена секция с формой (brif)

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,3 @@
-var button = document.querySelector('#button');
-var modal = document.querySelector('#modal');
-var close = document.querySelector('#close');
-button.addEventListener('click', function () {
-    modal.classList.add('modal_active');
-    setTimeout(closeModal, 5000);
+$(function() {
+    
 });
-function closeModal() {
-    modal.classList.remove('modal_active');
-};
-close.addEventListener('click', closeModal());
