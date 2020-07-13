27341f2c735c3fef5f87ad6b3e5cdbd96da13f92
quick-progress <georgiy.kuznetsov.19@mail.ru>
2020-07-13 23:58:29 +0600

    Добавлена секция с картой (без адаптива)

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
 $(function() {
-    
+    var mapboxHeight = $('.contacts').outerHeight();
+    var mapboxWidth = $('.contacts').outerWidth();
+    console.log(mapboxHeight);
+    $('.map').css('height', mapboxHeight+'px');
+    let docWidth = $(document).outerWidth();
+    console.log(docWidth);
+    if (docWidth <= 990) {
+        $('.map').css('flex-basis', mapboxWidth+'px');
+    };
 });
