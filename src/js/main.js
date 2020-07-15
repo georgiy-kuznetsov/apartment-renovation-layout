692eef01d60df0de397483d1893779fa3b847a56
quick-progress <georgiy.kuznetsov.19@mail.ru>
2020-07-16 00:36:09 +0600

    Добавлена разметка окна с услугами. Начата стилизация

--- src/js/main.js
+++ src/js/main.js
@@ -8,4 +8,34 @@ $(function() {
     if (docWidth <= 990) {
         $('.map').css('flex-basis', mapboxWidth+'px');
     };
+
+    // Инициализация WOW.js
+    new WOW().init();
+    $(document).ready(function() {
+        $('#brif-form').validate({
+            debug: true,
+        });
+        //Слайдер
+        $('.slider').slick({
+            infinite: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            prevArrow: $('.arrows__left'),
+            nextArrow: $('.arrows__right'),
+            responsive: [
+                {
+                breakpoint: 1240,
+                    settings: {
+                        slidesToShow: 2,
+                    }
+                },
+                {
+                    breakpoint: 840,
+                    settings: {
+                        slidesToShow: 1,
+                    }
+                },
+            ]
+        });
+    });
 });
